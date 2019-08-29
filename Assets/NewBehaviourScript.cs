39fc316f1ac2686afd4ec7255fde063655d0e380
melekk002 <54665311+melek002@users.noreply.github.com>
2019-08-29 16:55:51 +0100

    Add files via upload
    checkpoint by melek

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{ public void call(int a, int b)
+    { 
+        int c = a + b;
+        int e = a - b;
+        int f = a % b;
+        
+        Debug.Log(c);
+        Debug.Log(e);
+        Debug.Log(f);
+
+
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        call(17, 10);
+        call(10, 5);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+  
+   
+}
