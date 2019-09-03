31a9de26b7e929f5a77b49a4147e479ab1099e1b
melekk002 <54665311+melek002@users.noreply.github.com>
2019-09-04 00:03:18 +0100

    ARKANOID

--- Assets/ball.cs
+++ Assets/ball.cs
@@ -1,57 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
-
 
 public class ball : MonoBehaviour
 {
-    public Text scorerightText;
-    public Text scoreleftText;
-    int scoreRight;
-    int scoreLeft;
-    public float speed = 50;
-    float hitFactor(Vector2 ballPos, Vector2 racketPos, float racketHeight)
+    public float speed = 10.0f;
+    float  hitfactor(Vector2 Ballposition,Vector2 Racketposition,float Racketheight )
     {
-        return (ballPos.y - racketPos.y) / racketHeight;
+        return (Ballposition.x - Racketposition.x) / Racketheight;
     }
-    void OnCollisionEnter2D(Collision2D collision)
+
+     void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.name == "leftRacket")
+         if(collision.gameObject.name == "racket")
         {
-            float y = hitFactor(transform.position, collision.transform.position, collision.collider.bounds.size.y);
-            Vector2 dir = new Vector2(1, y).normalized;
-            GetComponent<Rigidbody2D>().velocity = dir * speed;
-        }
-
-        if (collision.gameObject.name == "rightRacket")
-        {
-            float y = hitFactor(transform.position, collision.transform.position, collision.collider.bounds.size.y);
-            Vector2 dir = new Vector2(-1, y).normalized;
-            GetComponent<Rigidbody2D>().velocity = dir * speed;
-
-            if (collision.gameObject.name == "Wallright")
-            {
-                //this line will just add 1 point to the score
-                scoreLeft++;
-                //this line will convert the int score variable to a string variable
-                scoreleftText.text = scoreLeft.ToString();
-            }
-            if (collision.gameObject.name == "Wallleft")
-            {
-                scoreRight++;
-                scorerightText.text = scoreRight.ToString();
-            }
+            float x = hitfactor(transform.position, collision.transform.position, collision.collider.bounds.size.x);
+            Vector2 d = new Vector2(x, 1).normalized;
+            GetComponent<Rigidbody>().velocity = d* speed;
+            Debug.Log("racket");
         }
     }
 
+
+
+
+    // Start is called before the first frame update
     void Start()
     {
-        GetComponent<Rigidbody2D>().velocity = Vector2.right * speed;
+        GetComponent<Rigidbody2D>().velocity = Vector2.up * speed;
     }
 
+    // Update is called once per frame
     void Update()
     {
-
+        
     }
 }
---
+++ Assets/racket.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class racket : MonoBehaviour
+{
+
+    public float speed = 10.0f;
+    public float h;
+    private void FixedUpdate()
+    {
+         h = Input.GetAxisRaw("Horizontal");
+        GetComponent<Rigidbody2D>().velocity = Vector2.right * h * speed;
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
